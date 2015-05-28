d9486e19ccb74e4f9ac3e3f0930e0eb591a0e9ad
JulianeW <JulianeW@users.noreply.github.com>
2015-05-28 17:11:22 +0200

    Update shared.h

--- shared.h
+++ shared.h
@@ -1,1 +1,34 @@
+/**
+ * @file shared.h
+ * Betriebssysteme Headerfile sender.c und empfaenger.c
+ *
+ * @author Claudia Baierl ic14b003 <claudia.baierl@technikum-wien.at>
+ * @author Rene Elsner ic14b016 <rene.elsner@technikum-wien.at>
+ * @author Juliane Piñeiro-Winkler ic14b059 <juliane.winkler@technikum-wien-at>
+ * @date 2015/06/05
+ *
+ * @version 1 
+ *
+ */
+
+#ifndef SHARED_H
+
+#define SHARED_H
+
+/*
+ * -------------------------------------------------------------- includes --
+ */
+ 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/*
+ * ------------------------------------------------------------- defines --
+ */
+ 
+
+
+#endif
+
 
