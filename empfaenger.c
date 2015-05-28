52b90f1aa901cdec68541247b24af4294bbd46cd
JulianeW <JulianeW@users.noreply.github.com>
2015-05-28 17:03:37 +0200

    Update empfaenger.c

--- empfaenger.c
+++ empfaenger.c
@@ -1,3 +1,23 @@
+/**
+ * @file sender.c
+ * Betriebssysteme Shared Memory
+ * Beispiel 3
+ *
+ * @author Claudia Baierl ic14b003 <claudia.baierl@technikum-wien.at>
+ * @author Rene Elsner ic14b016 <rene.elsner@technikum-wien.at>
+ * @author Juliane Piñeiro-Winkler ic14b059 <juliane.winkler@technikum-wien-at>
+ * @date 2015/06/05
+ *
+ * @version 1
+ *
+ */
+ 
+/*
+ * -------------------------------------------------------------- includes --
+ */
+
+#include "shared.h"
+
 int main (int argc, char *argv[]) {
 /* Holds the maximum amount of elements in the shared memory */
 	int liBufElems = 0;
